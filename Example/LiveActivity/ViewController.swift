6ce69459542fe90fa733ec94348102536d3b9ab6
Lyoid Lopes <lyoid.lopes@tapmango.com>
2023-12-16 19:22:51 -0500

    Inital release

--- Example/LiveActivity/ViewController.swift
+++ Example/LiveActivity/ViewController.swift
@@ -7,18 +7,79 @@
 //
 
 import UIKit
+import ActivityKit
+import SwiftUI
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // Add a label to the view
+        let label = UILabel()
+        label.text = "Hello, world!"
+        label.frame = CGRect(x: 50, y: 100, width: 200, height: 30) // Adjust the frame as needed
+        view.addSubview(label)
+
+        // Add a button to start the live activity
+        let button = UIButton(type: .system)
+        button.setTitle("Start Live Activity", for: .normal)
+        button.frame = CGRect(x: 50, y: 150, width: 200, height: 30) // Adjust the frame as needed
+        button.addTarget(self, action: #selector(startLiveActivityButtonPressed(_:)), for: .touchUpInside)
+        view.addSubview(button)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @objc func startLiveActivityButtonPressed(_ sender: UIButton) {
+        startLiveActivity()
+    }
+    
+    func startLiveActivity()  {
+        UIApplication.shared.registerForRemoteNotifications()
+        let center : UNUserNotificationCenter = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.sound , .alert , .badge ], completionHandler: { (granted, error) in
+            if ((error != nil)) { UIApplication.shared.registerForRemoteNotifications() }
+            else {
+                
+            }
+        })
+        Task {
+            do {
+                 let attribute = Widget_LiveActivityAttributes(
+                     name: "Bajrangbali"
+                 )
+                 
+                 let contentState = Widget_LiveActivityAttributes.LiveActivityData(
+                     emoji: "🤩"
+                 )
+                 
+                 
+                 let activity = try Activity<Widget_LiveActivityAttributes>.request(
+                     attributes: attribute,
+                     contentState: contentState,
+                     pushType: .token)
+                 for await data in activity.pushTokenUpdates {
+                     let token = data.map {String(format: "%02x", $0)}.joined()
+                     print("Live Activity Push Token: ", token)
+                 }
+           }
+           catch (let error) {
+               print(error.localizedDescription)
+           }
+        }
+    }
 
 }
 
+@available(iOS 16.4, *)
+struct Widget_LiveActivityAttributes: ActivityAttributes {
+    public typealias LiveActivityData = ContentState
+    public struct ContentState: Codable, Hashable {
+        var emoji: String
+    }
+    var name: String
+}
--- LiveActivity.podspec
+++ LiveActivity.podspec
@@ -1,42 +1,26 @@
-#
-# Be sure to run `pod lib lint LiveActivity.podspec' to ensure this is a
-# valid spec before submitting.
-#
-# Any lines starting with a # are optional, but their use is encouraged
-# To learn more about a Podspec see https://guides.cocoapods.org/syntax/podspec.html
-#
-
 Pod::Spec.new do |s|
   s.name             = 'LiveActivity'
-  s.version          = '0.1.0'
-  s.summary          = 'A short description of LiveActivity.'
+  s.version          = '1.0.1'
+  s.summary          = 'iOS LiveActivity Framework.'
 
-# This description is used to generate tags and improve search results.
-#   * Think: What does it do? Why did you write it? What is the focus?
-#   * Try to keep it short, snappy and to the point.
-#   * Write the description between the DESC delimiters below.
-#   * Finally, don't worry about the indent, CocoaPods strips it!
 
   s.description      = <<-DESC
-TODO: Add long description of the pod here.
+'An iOS framework developed to add Live Activity widget using Cocopods to exisiting Cordova Ionic Apps'
                        DESC
 
-  s.homepage         = 'https://github.com/Lyoid Lopes/LiveActivity'
-  # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
+  s.homepage         = 'https://github.com/demonguru18/LiveActivity'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
-  s.author           = { 'Lyoid Lopes' => 'lyoid.lopes@tapmango.com' }
-  s.source           = { :git => 'https://github.com/Lyoid Lopes/LiveActivity.git', :tag => s.version.to_s }
-  # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
+  s.author           = { 'Techhowdy' => 'thetechhowdy@tapmango.com' }
+  s.source           = { :git => 'https://github.com/demonguru18/LiveActivity.git', :tag => s.version.to_s }
 
-  s.ios.deployment_target = '10.0'
-
-  s.source_files = 'LiveActivity/Classes/**/*'
-  
+  s.ios.deployment_target = '16.4'
+  s.source_files = 'Source/**/*.swift'
+  s.swift_version = '5.0'
   # s.resource_bundles = {
   #   'LiveActivity' => ['LiveActivity/Assets/*.png']
   # }
 
   # s.public_header_files = 'Pod/Classes/**/*.h'
-  # s.frameworks = 'UIKit', 'MapKit'
+  s.frameworks = 'SwiftUI', 'WidgetKit'
   # s.dependency 'AFNetworking', '~> 2.3'
 end
